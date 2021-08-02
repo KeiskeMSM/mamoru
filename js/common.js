6aedc2acf9a254e0447296bf512a83927ab7e79f
KeisukeMSM <super.kiricub@gmail.com>
2021-08-02 11:41:08 +0900

    first commit

---
+++ js/common.js
@@ -0,0 +1,28 @@
+var isSP = true;
+var mql = window.matchMedia('screen and (max-width: 767px)');
+function checkBreakPoint(mql) {
+	if (mql.matches) {
+		isSP = true;
+	} else {
+		isSP = false;
+	}
+}
+mql.addListener(checkBreakPoint);
+checkBreakPoint(mql);
+
+$(function () {
+  $('.catch').on('click', function () {
+    if (isSP) {
+      $(this).next().slideToggle();
+      $(this).toggleClass('active');
+    }
+  });
+});
+
+
+$(function () {
+  $('.faq-item').on('click', function () {
+    $(this).find('.detail').slideToggle();
+    $('.accordion').toggleClass('active');
+  });
+});
